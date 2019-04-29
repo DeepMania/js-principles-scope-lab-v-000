aa14ad27548d82a17c014460917f22d2b326948c
Yana Ustsinovich <yustinovich@icloud.com>
2019-04-29 04:02:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName() {
+function upperCaseCustomerName(customerName) {
   let customerNameUpperCase = customerName.toUpperCase();
   return customerNameUpperCase;
 }
